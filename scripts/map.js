340cd69b5ac6f4b2fbadd4438f1269721ce84944
silvaisufi <silvaisufi26@gmail.com>
2020-04-19 15:38:49 +0200

    changes

--- scripts/map.js
+++ scripts/map.js
@@ -1,6 +1,30 @@
+let map = null;
+const button = document.getElementById('find-location');
 function initMap () {
-    const map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        zoom: 12,
     });
 }
+
+
+button.addEventListener("click", getLocation);
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            updateMap(position.coords.latitude, position.coords.longitude);
+        });
+    }
+}
+
+function updateMap(lat, lng) {
+    map.setCenter(new google.maps.LatLng(lat, lng));
+    const goldenGatePosition = {lat: lat,lng: lng};
+    const marker = new google.maps.Marker({
+        position: goldenGatePosition,
+        map: map,
+        title: '❤️ Position'
+        });
+}
+
+ 
